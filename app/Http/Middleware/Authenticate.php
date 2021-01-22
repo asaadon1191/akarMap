27533378dc0def26cc9c97085f901f3b926c14e5
unknown <asaadon22@gmail.com>
2021-01-22 02:08:03 +0200

    update before admin translation

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,23 +7,15 @@ use Request;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
+   
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) 
+        if (!$request->expectsJson()) 
         {
-            if (Request::is('/admin/*')) 
-                
-                return \route('admin.login');
-                else
+            if (Request::is(app()->getLocale().'/admin/*')) 
+                return route('admin.log');
+            else
                 return route('login');
         }
-        
-
     }
 }
